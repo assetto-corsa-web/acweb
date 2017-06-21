32f07ca7c77abc1ae38b681d6b87a00d8ef8839c
Thiago Macedo <macedot@users.noreply.github.com>
2017-06-21 14:59:52 -0300

    Missing comma and wrong AFTER

--- db/mig_1.0.3_1.0.4.sql
+++ db/mig_1.0.3_1.0.4.sql
@@ -3,9 +3,9 @@ ALTER TABLE `cars`
 ADD COLUMN `fixed_setup` varchar(100) NOT NULL AFTER `position`;
 
 ALTER TABLE `configurations`
-ADD COLUMN `legal_tyres` varchar(100) NOT NULL AFTER `track_config`
+ADD COLUMN `legal_tyres` varchar(100) NOT NULL AFTER `track_config`,
 ADD COLUMN `udp_plugin_local_port` int(10) NOT NULL AFTER `legal_tyres`,
-ADD COLUMN `udp_plugin_address` varchar(100) NOT NULL AFTER `udp_plugin_address`;
+ADD COLUMN `udp_plugin_address` varchar(100) NOT NULL AFTER `udp_plugin_local_port`;
 
 ALTER TABLE `weather`
 ADD COLUMN `wind_base_speed_min` int(10) NOT NULL AFTER `road_variation`,
